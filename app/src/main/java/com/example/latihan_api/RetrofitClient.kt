d0a67a21e6b99699ca8249b671491ad0f2fcce5f
RenaLaurentina <laurentinarena03@gmail.com>
2025-12-02 22:20:56 +0700

    latihan-api

---
+++ app/src/main/java/com/example/latihan_api/RetrofitClient.kt
@@ -0,0 +1,18 @@
+package com.example.latihan_api
+
+import com.example.latihan_api.repository.CatatanRepository
+import com.google.gson.Gson
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+object RetrofitClient {
+    private const val BASE_URL = "http://192.168.0.99:8000/api/"
+
+    val catatanRepository: CatatanRepository by lazy {
+        Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(CatatanRepository::class.java)
+    }
+}
